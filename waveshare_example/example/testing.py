e803684dd501b7aba7b8b40662dfa5a3aa522d86
Krish <krishkomugi@gmail.com>
2026-05-30 18:52:26 -0400

    update testing file

--- waveshare_example/example/testing.py
+++ waveshare_example/example/testing.py
@@ -31,7 +31,7 @@ try:
     im = Image.open("../pic/milkyWay.png")
     xsize, ysize = im.size
     sideL = im.crop((0,0,xsize//2,ysize//2))
-    im.paste(0,0)
+    im.paste(sideL,(0,120))
 
     #draw a cross onto the image
     draw = ImageDraw.Draw(im)
